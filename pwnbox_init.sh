bba5bac1b3fdc814fdd9415ccb8dae6a7dda973d
Sandev ✪ <53389569+s4ndev@users.noreply.github.com>
2024-07-17 04:45:30 +0800

    Update pwnbox_init.sh
    updated with organising

--- pwnbox_init.sh
+++ pwnbox_init.sh
@@ -2,8 +2,8 @@ sudo apt update
 sudo apt install xrdp xorgxrdp
 sudo rm /etc/xrdp/xrdp.ini
 sudo rm /etc/xrdp/sesman.ini
-sudo wget -O /etc/xrdp/xrdp.ini https://raw.githubusercontent.com/s4ndev/pwnbox-scripts/main/xrdp.ini
-sudo wget -O /etc/xrdp/sesman.ini https://raw.githubusercontent.com/s4ndev/pwnbox-scripts/main/sesman.ini
+sudo wget -O /etc/xrdp/xrdp.ini https://raw.githubusercontent.com/s4ndev/HTB-pwnbox-scripts/main/xrdp/xrdp.ini
+sudo wget -O /etc/xrdp/sesman.ini https://raw.githubusercontent.com/s4ndev/HTB-pwnbox-scripts/main/xrdp/sesman.ini
 echo "mate-session" > ~/.xsession
 sudo systemctl restart xrdp
 sudo systemctl enable xrdp
