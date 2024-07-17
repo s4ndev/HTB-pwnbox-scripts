eacd972ebe9256737f962e9d8793b55aa25405b3
Sandev ✪ <53389569+s4ndev@users.noreply.github.com>
2024-07-18 04:17:03 +0800

    Update example_client.sh
    massive improvements to the client-side script, RDP session performance increased massively.

--- example_client.sh
+++ example_client.sh
@@ -1,7 +1,8 @@
-## REQUIRED: freerdp, xquartz, sshpass
+## REQUIRED: freerdp, xquartz, sshpass (use brew on macOS!)
 
 #!/bin/sh
-TOKEN=INSERT TOKEN HERE
+TOKEN=INSERT_TOKEN_HERE
+PWNBOX_URL=https://labs.hackthebox.com/api/v4/pwnbox/status
 
 echo "Fetching instance details from Hack The Box..."
 RESP=$(curl -s -X GET -H "Authorization: Bearer $TOKEN" $PWNBOX_URL)
@@ -14,18 +15,14 @@ echo "Username: $USERNAME"
 echo "Password: $PASSWORD"
 echo "Host: $HOST"
 
+# Create SSH session without output, only to establish the tunnel and port forward 3389.
 sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -v -N -f -L 3389:localhost:3389 $USERNAME@$HOST
 
 # Give some time for the tunnel to establish
 sleep 3
 
-# Check if the tunnel is established
-echo "Checking if local port 3389 is listening..."
-netstat -an | grep 3389
-
 export DISPLAY=:0
-sleep 1
 open -a XQuartz
 sleep 2
 
-xfreerdp /v:localhost /u:$USERNAME /p:$PASSWORD /d:. /rfx /gfx:RFX:on,AVC420:on,small-cache:off,thin-client:off /gdi:sw
+xfreerdp /v:localhost /u:$USERNAME /p:$PASSWORD /d:. /compression /network:lan /gdi:sw /bpp:16 /cache:bitmap:on,glyph:off,offscreen:off /rfx /gfx:avc420 /dynamic-resolution /window-drag /menu-anims /fonts /rfx /frame-ack:8 /grab-keyboard /grab-mouse /scale:100 /themes
