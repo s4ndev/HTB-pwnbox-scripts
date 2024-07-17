5402f9a458f0a63e9aef522cf300a2a2261ec0ce
Sandev ✪ <53389569+s4ndev@users.noreply.github.com>
2024-07-18 00:54:55 +0800

    Update pwnbox_init.sh
    added a temporary testing feature to see if it executes successfully

--- pwnbox_init.sh
+++ pwnbox_init.sh
@@ -8,4 +8,6 @@ echo "mate-session" > ~/.xsession
 sudo systemctl restart xrdp
 sudo systemctl enable xrdp
 sudo gsettings set org.mate.Marco.general compositing-manager false
+# temporary testing feature to see if the script executes
+curl -X POST -d "executed=TRUE" https://posttestserver.dev/p/6mixd5d7p45crtyk/post
 sudo reboot
